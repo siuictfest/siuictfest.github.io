c09e774844485c635043232730bd80318384369f
siuictfest <siuictfest2017.com>
2017-10-25 22:37:57 +0600

    reg re open

--- js/acmfirebase.js
+++ js/acmfirebase.js
@@ -91,27 +91,27 @@ function getInputval(id){
 
 function SaveMessage(data){
     
-    
-    
-     
-    alert("Oops....Registration Closed !!!");
-    
-    document.getElementById('formid').reset();
-//    var PushID=acmTeamData.push();
-//    PushID.set(data).then(()=>{
-//          //Show alert 
-//    document.querySelector('.alert').style.display='block';
-//        
-//         alert("Your registration has been sucessfully completed.... :)");
 //    
-//    //Hide alert after 3 seconds
-//    setTimeout(()=>{
-//        document.querySelector('.alert').style.display='none';
 //    
-//    },3000);
+//     
+//    alert("Oops....Registration Closed !!!");
 //    
 //    document.getElementById('formid').reset();
-//    });
+    var PushID=acmTeamData.push();
+    PushID.set(data).then(()=>{
+          //Show alert 
+    document.querySelector('.alert').style.display='block';
+        
+         alert("Your registration has been sucessfully completed.... :)");
+    
+    //Hide alert after 3 seconds
+    setTimeout(()=>{
+        document.querySelector('.alert').style.display='none';
+    
+    },3000);
+    
+    document.getElementById('formid').reset();
+    });
 }
 
 
